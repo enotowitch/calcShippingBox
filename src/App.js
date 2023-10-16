32a6a125b8103b26d106ae42e87dc99fe171a30a
enotowitch <en.enotowitch@gmail.com>
2023-10-16 11:23:46 +0300

    1 com

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/data')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Response from the server:</p>
+        <p>{message}</p>
       </header>
     </div>
   );
 }
 
- App;
+export default App;
