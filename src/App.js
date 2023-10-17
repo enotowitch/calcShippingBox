323ff021e8252e3f96c61c9c14e3a68443fda02f
enotowitch <en.enotowitch@gmail.com>
2023-10-17 17:53:04 +0300

    com n

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(0);
+  const [resTime, setResTime] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(prev => prev + 1)
+    }, 1000);
+  }, [loading])
 
   useEffect(() => {
     fetch('http://localhost:5000/api/data')
       .then((response) => response.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => {
+        setMessage(data.message)
+      })
       .catch((error) => console.error(error));
   }, []);
+
+  useEffect(() => {
+    setResTime(loading)
+  }, [message])
 
   return (
     <div className="App">
       <header className="App-header">
+        <div>{loading}</div>
+        <div>{resTime}</div>
         <p>Response from the server:</p>
         <p>{message}</p>
       </header>
